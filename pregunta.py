8db7ecde5312e8efa66d35bed978010e00582ab6
joscanoga <51725572+joscanoga@users.noreply.github.com>
2022-09-21 17:38:51 -0500

    taller terminado

--- pregunta.py
+++ pregunta.py
@@ -12,10 +12,34 @@ espacio entre palabra y palabra.
 import pandas as pd
 
 
+
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df=pd.read_table("clusters_report.txt",header=None)
+    #df.columns=["cluster","cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"]
 
+    i=0
+    df.drop([ 0,1,2],axis=0, inplace=True)
+    tabla=[]
+    fila = {}
+    for i in range(len(df)):
+        #separar las palabras por espacios
+        line=df.iloc[i][0].strip().split()
+        if line[0].isnumeric():
+            if len(fila)>0:
+                tabla.append(fila)
+            fila={}
+            fila["cluster"]=int(line[0])
+            fila["cantidad_de_palabras_clave"]=int(line[1])
+            fila["porcentaje_de_palabras_clave"]=float(line[2].replace(",","."))
+            fila["principales_palabras_clave"]=" ".join(line[4:])
+        else:
+            fila["principales_palabras_clave"]+=" "+" ".join(line)
+            if fila["principales_palabras_clave"][-1]==".":
+                fila["principales_palabras_clave"]=fila["principales_palabras_clave"][:-1]
+    if len(fila) > 0:
+        tabla.append(fila)
+    df=pd.DataFrame(tabla)
+    #print(df)
     return df
+#ingest_data()
